d663166f3d5fb35e74fa211b892d0058b985a4eb
tswsxk <tongsw@mail.ustc.edu.cn>
2019-09-18 14:58:51 +0800

    添加谷歌搜索连接

--- conf.py
+++ conf.py
@@ -173,10 +173,10 @@ html_theme_options = {
         # ('伯克利深度学习课程', 'https://courses.d2l.ai/berkeley-stat-157/index.html', True, 'fas fa-user-graduate'),
         # ('PDF', 'https://zh.d2l.ai/d2l-zh.pdf', True, 'fas fa-file-pdf'),
         # ('Jupyter 记事本文件', 'https://zh.d2l.ai/d2l-zh.zip', True, 'fas fa-download'),
-        # ('讨论', 'https://discuss.gluon.ai/c/lecture?order=views', True, 'fab fa-discourse'),
         ('GitHub', 'https://github.com/tswsxk', True, 'fab fa-github'),
         ('CodeBook', 'https://github.com/tswsxk/CodeBook/tree/master/Handbook4DSAI', True, 'fas fa-book'),
         ('HomePage', 'https://tswsxk.github.io', True, 'fas fa-home'),
+        ('Search', 'http://scholar.hedasudi.com/', True, 'fab search'),
     ],
     'show_footer': True
 }
